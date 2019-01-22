0dfa3422184b689a826ffcd98e7322b7aac3f2ff
Smirennikov <evgenii.smirennikov@gmail.com>
2019-01-22 14:06:01 +0300

    fixed functional

--- frontend/js/lib.js
+++ frontend/js/lib.js
@@ -58,7 +58,7 @@ window.onscroll = function(){
     modalia.addEventListener('click', function(e){
       const target = e.target;
       if(target === modalia){
-        modalia.classList.toggle('active');
+        modalia.classList.remove('active');
       }
     })
   }
@@ -88,18 +88,78 @@ window.onscroll = function(){
   var butPrev = document.querySelector('.slider-buttons .but-prev');
   var slides = document.querySelectorAll('.section--our-clients img');
   var slidesLength = document.querySelectorAll('.section--our-clients img').length;
-
+  var numSlide = -1;
+  var nextSlide = 6;
   for(var i = 0; i < 5; i += 1){
     slides[i].classList.add('active')
   }
 
   butPrev.addEventListener('click',function(){
-    slides[0].classList.remove('active')
-    slides[6].classList.add('active')
+      slides[0].classList.add('active')
+      slides[6].classList.remove('active')
+
   })
   butNext.addEventListener('click',function(){
-    slides[0].classList.add('active')
-    slides[6].classList.remove('active')
+
+      slides[0].classList.remove('active')
+      slides[6].classList.add('active')
+
+
   })
 
-}()
+}();
+(function(){
+  // собираем все якоря; устанавливаем время анимации и количество кадров
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+      animationTime = 300,
+      framesCount = 20;
+
+anchors.forEach(function(item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function(e) {
+    // убираем стандартное поведение
+    e.preventDefault();
+
+    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top - 100;
+
+    // запускаем интервал, в котором
+    let scroller = setInterval(function() {
+      // считаем на сколько скроллить за 1 такт
+      let scrollBy = coordY / framesCount;
+
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  });
+});
+}());
+
+
+(function(){
+  var comments = document.querySelectorAll('.article-opinion');
+  var dots = document.querySelectorAll('.dots span');
+
+
+    dots[0].addEventListener('click' , function(){
+      dots[0].classList.add('active');
+      comments[0].classList.add('active');
+      dots[1].classList.remove('active');
+      comments[1].classList.remove('active');
+  })
+    dots[1].addEventListener('click' , function(){
+      dots[1].classList.add('active');
+      comments[1].classList.add('active');
+      dots[0].classList.remove('active');
+      comments[0].classList.remove('active');
+  })
+}())
