58d3d90f7b8ca4bc264622be834af90055a8031e
Smirennikov <evgenii.smirennikov@gmail.com>
2019-02-13 15:38:55 +0300

    add modal windows

--- public/js/lib.js
+++ public/js/lib.js
@@ -151,6 +151,9 @@ if (document.querySelector('.slider-buttons .but-next')) {
         slides[countMaxSlide].classList.remove('active');
         countMinSlide--;
         countMaxSlide -= 1;
+      } else if (countMinSlide === 0) {
+        slides[countMinSlide].classList.add('active');
+        slides[countMaxSlide].classList.remove('active');
       }
     });
     butNext.addEventListener('click', function () {
@@ -165,10 +168,130 @@ if (document.querySelector('.slider-buttons .but-next')) {
           countMaxSlide++;
         }
       }
-
-      console.log(countMaxSlide);
     });
   }();
+}
+
+if (document.querySelector('.section--completed-projects')) {
+  !function () {
+    var butNext = document.querySelector('.section--completed-projects .but-next');
+    var butPrev = document.querySelector('.section--completed-projects .but-prev');
+    var slides = document.querySelectorAll('.complete-project');
+    var slidesLength = document.querySelectorAll('.complete-project').length;
+    var countMinSlide = 0;
+    var countMaxSlide = 3;
+
+    for (var i = 0; i < 3; i += 1) {
+      slides[i].classList.add('active');
+    }
+
+    butPrev.addEventListener('click', function () {
+      if (countMinSlide > 0) {
+        slides[countMinSlide].classList.add('active');
+        slides[countMaxSlide].classList.remove('active');
+        countMinSlide -= 1;
+        countMaxSlide -= 1;
+      } else if (countMinSlide === 0) {
+        slides[countMinSlide].classList.add('active');
+        slides[countMaxSlide].classList.remove('active');
+      }
+    });
+    butNext.addEventListener('click', function () {
+      slides[countMinSlide].classList.remove('active');
+      slides[countMaxSlide].classList.add('active');
+
+      if (countMaxSlide < slidesLength) {
+        if (countMaxSlide === slidesLength - 1) {
+          countMaxSlide = slidesLength - 1;
+        } else {
+          countMinSlide++;
+          countMaxSlide++;
+        }
+      }
+    });
+  }();
+}
+
+if (document.querySelector('.section--completed-projects')) {
+  (function () {
+    var sectionPro = document.querySelector('.section--completed-projects');
+    var project = document.querySelectorAll('.complete-project');
+    var pModal = document.querySelector('.projects-modal');
+    var proModal = document.querySelectorAll('.block-open-project');
+    pModal.addEventListener('click', function (e) {
+      var target = e.target;
+
+      if (target === pModal) {
+        pModal.classList.remove('active');
+        proModal[0].classList.remove('active');
+        proModal[1].classList.remove('active');
+        proModal[2].classList.remove('active');
+        proModal[3].classList.remove('active');
+      }
+    });
+    project[0].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[0].classList.add('active');
+    });
+    project[1].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[1].classList.add('active');
+    });
+    project[2].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[2].classList.add('active');
+    });
+    project[3].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[3].classList.add('active');
+    });
+  })();
+}
+
+if (document.querySelector('.section--education-formats')) {
+  (function () {
+    var sectionPro = document.querySelector('.section--education-formats');
+    var project = document.querySelectorAll('.education-form');
+    var pModal = document.querySelector('.formats-modal');
+    var proModal = document.querySelectorAll('.block-open-format');
+    pModal.addEventListener('click', function (e) {
+      var target = e.target;
+
+      if (target === pModal) {
+        pModal.classList.remove('active');
+        proModal[0].classList.remove('active');
+        proModal[1].classList.remove('active');
+        proModal[2].classList.remove('active');
+        proModal[3].classList.remove('active');
+        proModal[4].classList.remove('active');
+        proModal[5].classList.remove('active');
+      }
+    });
+    project[0].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[0].classList.add('active');
+    });
+    project[1].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[1].classList.add('active');
+    });
+    project[2].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[2].classList.add('active');
+    });
+    project[3].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[3].classList.add('active');
+    });
+    project[4].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[4].classList.add('active');
+    });
+    project[5].addEventListener('click', function () {
+      pModal.classList.add('active');
+      proModal[5].classList.add('active');
+    });
+  })();
 }
 
 (function () {
