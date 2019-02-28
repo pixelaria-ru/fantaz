c2b3018b2bf39edf1ac2e43fe38d040dc13cba63
Smirennikov <evgenii.smirennikov@gmail.com>
2019-02-28 16:36:26 +0300

    fix fix fix

--- public/js/lib.js
+++ public/js/lib.js
@@ -297,37 +297,39 @@ if (document.querySelector('.section--education-formats')) {
   })();
 }
 
-(function () {
-  // собираем все якоря; устанавливаем время анимации и количество кадров
-  var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-      animationTime = 300,
-      framesCount = 20;
-  anchors.forEach(function (item) {
-    // каждому якорю присваиваем обработчик события
-    item.addEventListener('click', function (e) {
-      // убираем стандартное поведение
-      e.preventDefault(); // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+if (!document.querySelector('.section-discription-program')) {
+  (function () {
+    // собираем все якоря; устанавливаем время анимации и количество кадров
+    var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+        animationTime = 300,
+        framesCount = 20;
+    anchors.forEach(function (item) {
+      // каждому якорю присваиваем обработчик события
+      item.addEventListener('click', function (e) {
+        // убираем стандартное поведение
+        e.preventDefault(); // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
 
-      var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top - 100; // запускаем интервал, в котором
+        var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top - 100; // запускаем интервал, в котором
 
-      var scroller = setInterval(function () {
-        // считаем на сколько скроллить за 1 такт
-        var scrollBy = coordY / framesCount; // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-        // и дно страницы не достигнуто
+        var scroller = setInterval(function () {
+          // считаем на сколько скроллить за 1 такт
+          var scrollBy = coordY / framesCount; // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+          // и дно страницы не достигнуто
 
-        if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-          // то скроллим на к-во пикселей, которое соответствует одному такту
-          window.scrollBy(0, scrollBy);
-        } else {
-          // иначе добираемся до элемента и выходим из интервала
-          window.scrollTo(0, coordY);
-          clearInterval(scroller);
-        } // время интервала равняется частному от времени анимации и к-ва кадров
+          if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+            // то скроллим на к-во пикселей, которое соответствует одному такту
+            window.scrollBy(0, scrollBy);
+          } else {
+            // иначе добираемся до элемента и выходим из интервала
+            window.scrollTo(0, coordY);
+            clearInterval(scroller);
+          } // время интервала равняется частному от времени анимации и к-ва кадров
 
-      }, animationTime / framesCount);
+        }, animationTime / framesCount);
+      });
     });
-  });
-})();
+  })();
+}
 
 if (document.querySelector('.article-opinion')) {
   (function () {
